9bc82b0f66d4b17df059dd2adc0a116dfc6d3a8b
charrus <charlie@rusbridger.com>
2023-07-13 21:50:06 +0100

    Use request, make port optional

--- sendsmspython3.py
+++ sendsmspython3.py
@@ -1,33 +1,39 @@
 #!/usr/bin/python
 
-# coding: utf8
-
-from time import gmtime, strftime
-import urllib
-import pycurl
+import time
+import requests
 import sys
-import urllib.parse
 
-#host = '192.168.8.1'
-host = '192.168.0.1'
-port = '80'
+host = "192.168.0.1"
+port = "80"
 
-text=sys.argv[1]
-text=text.encode("utf-16-be")
-msg="".join("{:02x}".format(c) for c in text)
+if port:
+    origin = f"http://{host}:{port}"
+else:
+    origin = f"http://{host}"
 
-#number="+79***"
-number=sys.argv[2]
+text = sys.argv[1]
+text = text.encode("utf-16-be")
+msg = "".join("{:02x}".format(c) for c in text)
 
-time=strftime("%y;%m;%d;%H;%M;%S;+5", gmtime())
+number = sys.argv[2]
 
-url = 'http://'+host+':'+port+'/goform/goform_set_cmd_process'
-post = urllib.parse.urlencode({'notCallback' : 'true', 'goformId' : 'SEND_SMS', 'isTest' : 'false','Number' : number, 'sms_time': time, 'MessageBody': msg, 'encode_type' : 'UNICODE', 'ID' : '-1'})
-c = pycurl.Curl()
-c.setopt(pycurl.URL, url)
-c.setopt(pycurl.REFERER, 'http://'+host+':'+port+'/index.html')
-c.setopt(pycurl.VERBOSE, 0)
-c.setopt(pycurl.POST, 1)
-c.setopt(pycurl.POSTFIELDS, post)
-c.perform()
-print("\n")
+time = time.strftime("%y;%m;%d;%H;%M;%S;%z", time.localtime())
+
+url = f"{origin}/goform/goform_set_cmd_process"
+headers = {
+    "Referer": f"{origin}/index.html",
+    "Origin": f"{origin}",
+}
+payload = {
+    "isTest": "false",
+    "goformId": "SEND_SMS",
+    "notCallback": "true",
+    "Number": number,
+    "sms_time": time,
+    "MessageBody": msg,
+    "ID": "-1",
+    "encode_type": "UNICODE",
+}
+r = requests.post(url, headers=headers, data=payload)
+print(r.text)
